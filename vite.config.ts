2be4cfa663aa69fedcabbf3489e7450bf4dbc914
Dheeraj <ds5907311@gmail.com>
2026-05-29 15:17:34 +0530

    add path alias resolution in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,12 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import { defineConfig } from 'vite';
 import viteReact from '@vitejs/plugin-react';
 import { nitro } from 'nitro/vite';
+import path from 'path';
 
 export default defineConfig({
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
   plugins: [tanstackStart(
     {
       server: {
